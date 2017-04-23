7ab862bbe40b9c0957b39ff96428b14f7151a059
szilardinho <szilardinhog@gmail.com>
2017-04-23 00:59:47 -0400

    Pokemon class created

---
+++ Pokemon.swift
@@ -0,0 +1,43 @@
+//
+//  Pokemon.swift
+//  pokedexApp
+//
+//  Created by Szi Gabor on 4/23/17.
+//  Copyright © 2017 Szi. All rights reserved.
+//
+
+import Foundation
+
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokemonID: Int!
+    
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokemonID: Int {
+     
+        return _pokemonID
+    }
+    
+    init(name: String, pokemonID: String){
+        
+        self._name = name
+        self._pokemonID = pokemonID
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
